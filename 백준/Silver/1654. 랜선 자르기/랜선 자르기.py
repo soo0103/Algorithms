3b18feb3e6c771cd43739aa4ebe58fc2c2396b46
기현 <k1hyxn01@naver.com>
2025-02-01 13:02:23 +0900

    [Silver II] Title: 랜선 자르기, Time: 80 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,24 @@
+import sys
+
+K, N = map(int, sys.stdin.readline().split())
+arr = []
+
+for _ in range(K):
+    arr.append(int(sys.stdin.readline()))
+
+start = 1
+end = max(arr)
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+
+    for i in arr:
+        cnt += i // mid
+
+    if cnt >= N:
+        start = mid + 1
+    else:
+        end = mid - 1
+        
+print(end)
