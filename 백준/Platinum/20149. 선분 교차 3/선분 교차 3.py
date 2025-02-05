1e09d17a0411e19adbcdd1adc7c0d2b4f690ab2c
기현 <k1hyxn01@naver.com>
2025-02-05 15:22:24 +0900

    [Platinum IV] Title: 선분 교차 3, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/20149. 선분 교차 3/선분 교차 3.py
+++ 백준/Platinum/20149. 선분 교차 3/선분 교차 3.py
@@ -16,7 +16,7 @@ def ccw(x1, y1, x2, y2, x3, y3):
 def isIntersect(x1, y1, x2, y2, x3, y3, x4, y4):
     if ccw(x1, y1, x3, y3, x4, y4) * ccw(x2, y2, x3, y3, x4, y4) == 0:
         if ccw(x3, y3, x1, y1, x2, y2) * ccw(x4, y4, x1, y1, x2, y2) == 0:
-            if (x1,y1) > (x2, y2):
+            if (x1, y1) > (x2, y2):
                 x1, x2 = x2, x1
                 y1, y2 = y2, y1
 
@@ -43,8 +43,8 @@ if __name__ == "__main__":
         print(1)
 
         try:
-            x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x3 * y4 - x4 * y3) * (x1-x2)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
-            y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (x3 * y4 - y3 * x4) * (y1-y2)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+            x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x3 * y4 - x4 * y3) * (x1 - x2)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+            y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (x3 * y4 - y3 * x4) * (y1 - y2)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
 
             print(x, y)
         except:
