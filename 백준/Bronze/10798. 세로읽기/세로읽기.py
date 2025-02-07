37dd9ce945bbb5e8488186e0b7bc5f90896272b2
기현 <k1hyxn01@naver.com>
2025-02-07 11:01:34 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,8 @@
+import sys
+
+words = [sys.stdin.readline().strip() for _ in range(5)]
+
+for i in range(15):
+    for j in range(5):
+        if i < len(words[j]):
+            print(words[j][i], end = '')
