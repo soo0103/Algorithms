d94939577c9b8695e41217151e15693f3782fdcd
기현 <k1hyxn01@naver.com>
2025-02-07 10:31:30 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,10 @@
+import sys
+
+word = sys.stdin.readline().strip()
+
+croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+for c in croatia:
+    word = word.replace(c, "c")
+
+print(len(word))
