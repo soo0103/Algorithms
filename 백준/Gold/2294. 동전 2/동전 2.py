474267e63eaecfc82510284d6a5b4621ef307666
기현 <k1hyxn01@naver.com>
2025-02-22 20:51:04 +0900

    [Gold V] Title: 동전 2, Time: 252 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -8,7 +8,7 @@ dp[0] = 0
 
 for coin in coins:
    for i in range(coin, k + 1):
-       dp[i] = min(dp[i], dp[i- coin] + 1)
+       dp[i] = min(dp[i], dp[i - coin] + 1)
 
 if dp[k] == 10001:
     print(-1)
