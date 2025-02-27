4335d9abc1889bd850c2db5ba1bd655f8f02e57e
기현 <k1hyxn01@naver.com>
2025-02-27 14:03:59 +0900

    [Silver V] Title: 단어 나누기, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,18 @@
+import sys
+
+
+word = list(sys.stdin.readline().strip())
+
+words = []
+
+for i in range(1, len(word) - 1):
+    for j in range(i + 1, len(word)):
+        a = word[:i][::-1]
+        b = word[i:j][::-1]
+        c = word[j:][::-1]
+
+        words.append(a + b + c)
+        
+words.sort()
+
+print(*words[0], sep = '')
