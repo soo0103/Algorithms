df4937ce09db2d6edbcbf4f4114ea5578e6f4274
기현 <k1hyxn01@naver.com>
2025-02-27 09:44:08 +0900

    [Gold IV] Title: 전화번호 목록, Time: 168 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,20 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+
+    numbers = [sys.stdin.readline().strip() for _ in range(n)]
+    numbers.sort()
+
+    flag = True
+
+    for i in range(n - 1):
+        if numbers[i] in numbers[i + 1][:len(numbers[i])]:
+            print("NO")
+            flag = False
+            break
+
+    if flag:
+        print("YES")
