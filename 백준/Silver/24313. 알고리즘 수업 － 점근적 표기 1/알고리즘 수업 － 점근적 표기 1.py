9947e16e304ef4ea154cb879c2677939f8c6f65c
기현 <k1hyxn01@naver.com>
2025-03-03 20:15:22 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -0,0 +1,10 @@
+import sys
+
+a1, a0 = map(int, sys.stdin.readline().split())
+c = int(sys.stdin.readline())
+n0 = int(sys.stdin.readline())
+
+if a1 <= c and a1 * n0 + a0 <= c * n0:
+    print(1)
+else:
+    print(0)
