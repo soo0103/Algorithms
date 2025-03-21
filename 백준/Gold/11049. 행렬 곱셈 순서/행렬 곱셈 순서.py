458f63950ac274a921a8273bc89643afdfc68cb7
기현 <k1hyxn01@naver.com>
2025-03-21 10:56:44 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 4992 ms, Memory: 37428 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -2,20 +2,25 @@ import sys
 
 INF = int(1e9)
 
-N = int(sys.stdin.readline())
-matrices = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+def minmult():
+    for d in range(1, N + 1):
+        for i in range(1, N - d + 1):
+            j = i + d
+            for k in range(i, j):
+                value = dp[i][k] + dp[k + 1][j] + p[i] * p[k + 1] * p[j + 1]
+                if dp[i][j] > value:
+                    dp[i][j] = value
 
-dp = [[INF] * N for _ in range(N)]
+    print(dp[1][N])
 
-for i in range(N):
-    dp[i][i] = 0
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    matrices = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-for d in range(N - 1):
-    for i in range(N - d - 1):
-        j = i + d + 1
-        for k in range(i, j):
-            value = dp[i][k] + dp[k + 1][j] + matrices[i][0] * matrices[k][1] * matrices[j][1]
-            if dp[i][j] > value:
-                dp[i][j] = value
+    dp = [[INF] * (N + 1) for _ in range(N + 1)]
+    p = [0, matrices[0][0]] + [m[1] for m in matrices]
 
-print(dp[0][N - 1])
+    for i in range(N + 1):
+        dp[i][i] = 0
+
+    minmult()
