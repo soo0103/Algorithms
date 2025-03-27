3da46799571825d393b450db6b11d0fccefa5093
기현 <k1hyxn01@naver.com>
2025-03-27 10:26:54 +0900

    [Gold III] Title: 탄성 충돌, Time: 536 ms, Memory: 41796 KB -BaekjoonHub

--- 백준/Gold/16891. 탄성 충돌/탄성 충돌.py
+++ 백준/Gold/16891. 탄성 충돌/탄성 충돌.py
@@ -1,16 +1,20 @@
 import sys
 from fractions import Fraction
 
+C = 300000
+
 N = int(sys.stdin.readline())
 
-m = N ** 2
-u1 = 0
-u2 = -100000
+m2 = N ** 2
+u1, u2 = 0, -C
 cnt = 0
 
 while 1:
-    v1 = Fraction((1 - m), (1 + m)) * u1 + Fraction((2 * m), (1 + m)) * u2
-    v2 = Fraction(2, (1 + m)) * u1 - Fraction((1 - m), (1 + m)) * u2
+    if u2 - u1 >= 0:
+        break
+
+    v1 = Fraction((1 - m2), (1 + m2)) * u1 + Fraction((2 * m2), (1 + m2)) * u2
+    v2 = Fraction(2, (1 + m2)) * u1 - Fraction((1 - m2), (1 + m2)) * u2
     cnt += 1
 
     u1 = v1
@@ -20,8 +24,5 @@ while 1:
         if u1 < 0:
             cnt += 1
         u1 = -u1
-        
-    if u2 - u1 >= 0:
-        break
 
 print(cnt)
