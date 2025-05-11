1a576cb85374247ec24a43cfd5e0d529a530d902
기현 <k1hyxn01@naver.com>
2025-05-11 11:23:37 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -3,15 +3,11 @@ import sys
 N, K = map(int, sys.stdin.readline().split())
 temperature = list(map(int, sys.stdin.readline().split()))
 
-left = 0
-right = K - 1
-comp = sum(temperature[left:right + 1])
+comp = sum(temperature[0:K])
 max_temperature = comp
 
-while right < len(temperature) - 1:
-    right += 1
-    comp = comp - temperature[left] + temperature[right]
-    left += 1
+for i in range(N - K):
+    comp += temperature[i + K] - temperature[i]
     
     if comp > max_temperature:
         max_temperature = comp
