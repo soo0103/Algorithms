3f6759de0471d4af430efd79854d33c0a0a64d29
기현 <k1hyxn01@naver.com>
2025-05-18 12:29:05 +0900

    [Silver II] Title: 이동하기, Time: 712 ms, Memory: 78872 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,15 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+maze = [[0] * M]
+
+for _ in range(N):
+    maze.append([0] + list(map(int, sys.stdin.readline().split())))
+
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + maze[i][j]
+
+print(dp[N][M])
