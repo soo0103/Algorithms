af5f9a4705b8ab7fb60d4c304e78d3cf910010bc
기현 <k1hyxn01@naver.com>
2025-08-04 09:50:55 +0900

    [Gold V] Title: 전깃줄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = []
+dp = [1] * N
+
+for _ in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    arr.append((a, b))
+
+arr.sort()
+
+for i in range(1, N):
+    for j in range(0, i):
+        if arr[j][1] < arr[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+     
+print(N - max(dp))
